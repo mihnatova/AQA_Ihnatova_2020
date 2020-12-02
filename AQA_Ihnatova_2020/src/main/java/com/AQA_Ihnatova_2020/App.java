1e6906eaf4abbba95e69f1cb50329b5ea68381e2
Mariia Ihnatova <mariia_ihnatova@MacBook-Air-Mariia.local>
2020-12-02 20:57:09 +0200

    add AQA_Ihnatova_2020/ project

---
+++ AQA_Ihnatova_2020/src/main/java/com/AQA_Ihnatova_2020/App.java
@@ -0,0 +1,13 @@
+package com.AQA_Ihnatova_2020;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ AQA_Ihnatova_2020/src/test/java/com/AQA_Ihnatova_2020/AppTest.java
@@ -0,0 +1,20 @@
+package com.AQA_Ihnatova_2020;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
